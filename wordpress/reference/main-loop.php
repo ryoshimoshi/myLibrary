a2541b68c25ad4ffe140f8b691cdfa390c149d3f
ryoshimoshi <r.yoshimo.lfc@gmail.com>
2017-11-27 14:24:48 +0900

    third commit

--- wordpress/reference/main-loop.php
+++ wordpress/reference/main-loop.php
@@ -4,3 +4,18 @@
 <?php endwhile; else: ?>
     <span>記事がありません。</span>
 <?php endif; ?>
+
+
+<!-- usually main-lop -->
+<?php if ( have_posts() ):  while ( have_posts() ) : the_post(); ?>
+    <a href="<?php echo get_the_permalink(); ?>">
+        <!-- thumbnail -->
+        <?php if (has_post_thumbnail()): $image_url = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'medium'); ?>
+            <div style="background-image: url('<?php echo $image_url[0]; ?>')"></div>
+        <?php else: ?>
+            <div style="background-image: url('./test.jpg')"></div>
+        <?php endif; ?>
+    </a>
+<?php endwhile; else: ?>
+    <span>記事がありません。</span>
+<?php endif; ?>
